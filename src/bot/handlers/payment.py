9b201c89c4f105585d681da88b708efad7bb6cdb
Artemka1806 <tyschenkoartem2580@gmail.com>
2025-07-21 15:52:28 +0300

    Remove unnecessary session close call in successful payment handler

--- src/bot/handlers/payment.py
+++ src/bot/handlers/payment.py
@@ -44,7 +44,6 @@ async def success_payment_handler(message: Message, bot: Bot):
                     payment.status = "error"
                     await payment.save()
                     await bot.refund_star_payment(message.from_user.id, message.successful_payment.telegram_payment_charge_id)
-                    await session.close()
 
     payment.status = "completed"
     await payment.save()
